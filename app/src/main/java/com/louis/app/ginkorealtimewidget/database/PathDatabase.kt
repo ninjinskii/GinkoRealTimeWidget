a835c7981568472ac775c26f4c266cfff20ad6d0
ninjinski <louis.bailly1@etu.univ-lorraine.fr>
2020-07-10 09:44:19 +0200

    Add database singleton sample

--- app/src/main/java/com/louis/app/ginkorealtimewidget/database/PathDatabase.kt
+++ app/src/main/java/com/louis/app/ginkorealtimewidget/database/PathDatabase.kt
@@ -35,8 +35,37 @@ abstract class PathDatabase : RoomDatabase() {
             }
         }
     }
-
-    fun destroyInstance() {
-        INSTANCE = null
-    }
 }
+
+//@Database(entities = [GardenPlanting::class, Plant::class], version = 1, exportSchema = false)
+//@TypeConverters(Converters::class)
+//abstract class AppDatabase : RoomDatabase() {
+//    abstract fun gardenPlantingDao(): GardenPlantingDao
+//    abstract fun plantDao(): PlantDao
+//
+//    companion object {
+//
+//        // For Singleton instantiation
+//        @Volatile private var instance: AppDatabase? = null
+//
+//        fun getInstance(context: Context): AppDatabase {
+//            return instance ?: synchronized(this) {
+//                instance ?: buildDatabase(context).also { instance = it }
+//            }
+//        }
+//
+//        // Create and pre-populate the database. See this article for more details:
+//        // https://medium.com/google-developers/7-pro-tips-for-room-fbadea4bfbd1#4785
+//        private fun buildDatabase(context: Context): AppDatabase {
+//            return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
+//                .addCallback(object : RoomDatabase.Callback() {
+//                    override fun onCreate(db: SupportSQLiteDatabase) {
+//                        super.onCreate(db)
+//                        val request = OneTimeWorkRequestBuilder<SeedDatabaseWorker>().build()
+//                        WorkManager.getInstance(context).enqueue(request)
+//                    }
+//                })
+//                .build()
+//        }
+//    }
+//}
